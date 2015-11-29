d1dee9eee9e23888d38791c25a9c56fb7a7880be
Gabe Fernando <gef756@gmail.com>
2015-11-29 18:10:50 -0500

    ENH: implement step 2

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -9,3 +9,14 @@ describe('controllers', function() {
   });
 
 });
+
+describe('PhoneListCtrl', function() {
+  beforeEach(module('phonecatApp'));
+  it('should create "phones" model with 3 phones',
+      inject(function($controller) {
+        var scope = {};
+        var ctrl = $controller('PhoneListCtrl', {$scope: scope});
+        expect(scope.phones.length).toBe(3);
+        expect(scope.name).toBe('World');
+      }));
+});
