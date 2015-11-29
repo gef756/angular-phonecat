76635f2a7fead8df87906918dd4229b216d4dff3
Gabe Fernando <gef756@gmail.com>
2015-11-29 18:48:09 -0500

    ENH: implement step 3, ex2: display query in title

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -15,10 +15,12 @@ describe('PhoneCat App', function() {
     beforeEach(function() {
       browser.get('app/index.html');
     });
+
+    var phoneList = element.all(by.repeater('phone in phones'));
+    var query = element(by.model('query'));
+
     it('should filter the phone list as a user types into the search box',
         function() {
-          var phoneList = element.all(by.repeater('phone in phones'));
-          var query = element(by.model('query'));
           expect(phoneList.count()).toBe(3);
 
           query.sendKeys('nexus');
@@ -28,5 +30,13 @@ describe('PhoneCat App', function() {
           query.sendKeys('motorola');
           expect(phoneList.count()).toBe(2);
         });
+
+    it('should display the current filter value in the title bar', function() {
+      query.clear();
+      expect(browser.getTitle()).toMatch(/Google Phone Gallery:\s*$/);
+
+      query.sendKeys('nexus');
+      expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/);
+    });
   });
 });
