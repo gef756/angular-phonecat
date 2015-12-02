71572d230b289bf654bdeee556339dc8662ff2fd
Gabe Fernando <gef756@gmail.com>
2015-12-01 21:59:35 -0500

    FIX: step 5: add dependency annotations to allow for minification

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,11 +4,13 @@
 
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
-  $http.get('phones/phones.json').success(function(data) {
-    $scope.phones = data;
-  });
+// annotate function with dependency names
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
 
-  $scope.orderProp = 'age';
-  $scope.name = "World";
-});
+    $scope.orderProp = 'age';
+    $scope.name = "World";
+}]);
