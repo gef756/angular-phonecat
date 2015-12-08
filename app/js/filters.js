219ad0bbbf61d3723c17546e9a7872aa89e7e2aa
Gabe Fernando <gef756@gmail.com>
2015-12-08 14:38:53 -0500

    ENH: implement step 9 (filters)

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,3 +1,9 @@
 'use strict';
 
 /* Filters */
+angular.module('phonecatFilters', []).filter('checkmark', function() {
+  return function(input) {
+    // convert True / False to check mark or X
+    return input ? '\u2713': '\u2718';
+  };
+});
