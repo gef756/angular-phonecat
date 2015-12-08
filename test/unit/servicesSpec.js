c177849686695ca39e3e433590498d63d45631fe
Gabe Fernando <gef756@gmail.com>
2015-12-08 15:58:40 -0500

    TST: add test for phone factory

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -3,5 +3,10 @@
 /* jasmine specs for services go here */
 
 describe('service', function() {
+  beforeEach(module('phonecatApp'));
 
+  // Test service availability
+  it('should have a Phone factory', inject(function(Phone) {
+    expect(Phone).toBeDefined();
+  }));
 });
