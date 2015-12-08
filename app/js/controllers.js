de11c55529ed4bc11624f62d344ddd3a6e7853df
Gabe Fernando <gef756@gmail.com>
2015-12-08 15:11:04 -0500

    ENH: implement part 10 (click to switch main image)

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -22,5 +22,10 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
       .get('phones/' + $routeParams.phoneId + '.json')
       .success(function(data) {
         $scope.phone = data;
-      })
+        $scope.mainImageUrl = data.images[0];
+      });
+
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl;
+    };
   }]);
