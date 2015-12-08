9e9189e75f3b69c3f665903b9eadee9b96214d00
Gabe Fernando <gef756@gmail.com>
2015-12-08 15:59:02 -0500

    ENH: implement step 11 (REST and Custom Services)

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,25 +5,20 @@
 var phonecatControllers = angular.module('phonecatControllers', []);
 
 // annotate function with dependency names
-phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('phones/phones.json').success(function(data) {
-      $scope.phones = data;
-      // $scope.phones = data.splice(0, 5); // limit to top 5 results
-    });
+phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
+  function ($scope, Phone) {
+    $scope.phones = Phone.query();
+    // $scope.phones = data.splice(0, 5); // limit to top 5 results
 
     $scope.orderProp = 'age';
     $scope.name = "World";
 }]);
 
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
-  '$http', function($scope, $routeParams, $http) {
-    $http
-      .get('phones/' + $routeParams.phoneId + '.json')
-      .success(function(data) {
-        $scope.phone = data;
-        $scope.mainImageUrl = data.images[0];
-      });
+  'Phone', function($scope, $routeParams, Phone) {
+    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function (phone) {
+        $scope.mainImageUrl = phone.images[0];
+    });
 
     $scope.setImage = function(imageUrl) {
       $scope.mainImageUrl = imageUrl;
